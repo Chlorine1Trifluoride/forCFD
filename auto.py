80e0c27515a8d6b1773b594179e643d5bf91ee73
Chlorine Trifluoride <s2510423@siheung.hs.kr>
2025-10-02 05:44:10 +0000

    xlsx 파일 제작 성공 + auto.py 오류개선

--- auto.py
+++ auto.py
@@ -41,50 +41,55 @@ if choice == 1:
 elif choice ==6:
 
   results = []
-  for folder in os.listdir(cases_root):
-    if folder.endswith("deg"):
-      lift_list = []
-      drag_list = [] 
-      forces = os.path.join(cases_root, folder, "postProcessing", "Forces", "0", "forces.dat")
-      with open(forces, "r") as f:
-        for line in f:
-          if line.startswith("#") or line.strip() == "":
-            continue
-          vectors = re.findall(r"\(([^()]+)\)", line)
-          f_a = vectors[0].strip().split()
-          f_b = vectors[1].strip().split()
-          drag = float(f_a[1])+float(f_b[1])
-          lift = float(f_a[2])+float(f_b[2])
-          drag_list.append(drag)
-          lift_list.append(lift)
-      drag_arr = np.array(drag_list)
-      lift_arr = np.array(lift_list)
-      drag_mean = np.mean(drag_arr)
-      lift_mean = np.mean(lift_arr)
-      drag_std = np.std(drag_arr)
-      lift_std = np.std(lift_arr)
-      if folder.startswith("m"):
-        results.append({
-          "Case": folder[1:],
-          "Lift Mean": lift_mean,
-          "Lift Std": lift_std,
-          "Drag Mean": drag_mean,
-          "Drag Std": drag_std
-        })
-      else:
-        results.append({
-          "Case": folder,
-          "Lift Mean": lift_mean,
-          "Lift Std": lift_std,
-          "Drag Mean": drag_mean,
-          "Drag Std": drag_std
-        })
+  folders = sorted(
+    [f for f in os.listdir(cases_root) if f.endswith("deg")], 
+    key=lambda x: float(x.replace("deg", "").replace("m",""))
+  )
+  
+  for folder in folders:
+    lift_list = []
+    drag_list = [] 
+    forces = os.path.join(cases_root, folder, "postProcessing", "Forces", "0", "forces.dat")
+    with open(forces, "r") as f:
+      for line in f:
+        if line.startswith("#") or line.strip() == "":
+          continue
+        vectors = re.findall(r"\(([^()]+)\)", line)
+        f_a = vectors[0].strip().split()
+        f_b = vectors[1].strip().split()
+        drag = float(f_a[1])+float(f_b[1])
+        lift = float(f_a[2])+float(f_b[2])
+        drag_list.append(drag)
+        lift_list.append(lift)
+    drag_arr = np.array(drag_list)
+    lift_arr = np.array(lift_list)
+    drag_mean = np.mean(drag_arr)
+    lift_mean = np.mean(lift_arr)
+    drag_std = np.std(drag_arr)
+    lift_std = np.std(lift_arr)
+    if folder.startswith("m"):
+      results.append({
+        "Case": folder[1:],
+        "Lift Mean": lift_mean,
+        "Lift Std": lift_std,
+        "Drag Mean": drag_mean,
+        "Drag Std": drag_std
+      })
+    else:
+      results.append({
+        "Case": folder,
+        "Lift Mean": lift_mean,
+        "Lift Std": lift_std,
+        "Drag Mean": drag_mean,
+        "Drag Std": drag_std
+      })
   df = pd.DataFrame(results)
   df.to_excel("10423정승환-CFDsimulation_Result.xlsx", index=False)  
 
 
 elif choice > 1 and choice < 6:
   foldername = input("enter your FOLDER name: ")
+
 
 
   if choice == 2 or choice == 4:
@@ -101,34 +106,35 @@ elif choice > 1 and choice < 6:
           if folder.endswith("deg"):
             target = os.path.join(cases_root, folder, foldername, filename)
             shutil.copy2(source, target)
-            print(f"Successfully Updated {filename} in {folder}")
+            print(f"Successfully Updated {filename} in {foldername} in {folder}")
 
 
     elif choice ==4:
       for folder in os.listdir(cases_root):
         if folder.endswith("deg"):
-          target = os.path.join(cases_root, folder, foldername)
+          target = os.path.join(cases_root, folder, foldername, filename)
 
           if os.path.exists(target):
             os.rmtree(target)
-            print(f"Successfully Deleted {folder}")
+            print(f"Successfully Deleted {filename} in {foldername} in {folder}")
             
           else:
-            print(f"[FATAL ERROR] DIRECTORY NOT FOUND: {target}")
+            print(f"[FATAL ERROR] SOURCE NOT FOUND: {target}")
 
 
 
   elif choice == 3 or choice == 5:
     if choice == 3:
+      source = os.path.join(cases_root, "CaseTemplate", foldername)
       if not os.path.exists(os.path.join(cases_root, "CaseTemplate", foldername)):
-        source = os.path.join(cases_root, "CaseTemplate", foldername)
         print(f"[FATAL ERROR] SOURCE NOT FOUND: {source}")
 
       else:
+        
         for folder in os.listdir(cases_root):
           if folder.endswith("deg"):
             target = os.path.join(cases_root, folder, foldername)
-            shutil.copytree(cases_root, folder, foldername, target)
+            shutil.copytree(source, target)
             print(f"Successfully Updated {foldername} in {folder}")
 
 
@@ -138,7 +144,7 @@ elif choice > 1 and choice < 6:
           target = os.path.join(cases_root, folder, foldername)
 
           if os.path.exists(target):
-            os.remove(target)
+            shutil.rmtree(target)
             print(f"Successfully Deleted {foldername} in {folder}")
             
           else:
