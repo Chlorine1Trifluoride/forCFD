177fb1cfdf131c922592baa864fb32731d1e3fad
Chlorine Trifluoride <s2510423@siheung.hs.kr>
2025-10-27 08:37:49 +0900

    살려주세요

--- start.py
+++ start.py
@@ -3,8 +3,8 @@ import sys
 def runpip(cmd): sp.run([sys.executable, "-m", "pip"] + cmd.split(), shell=False)
 try:
     import HEIL, menu
-    menu.box("[start]", [])
-except: print("[start]")
+    menu.box("[ start ]", [])
+except: print("[ start ]")
 runpip("--version")
 runpip("install -r requirements.txt")
 sp.run([sys.executable, "auto.py"], shell=False)
