4268297330898157f0413979b63e298ccfa10c4a
Chlorine Trifluoride <s2510423@siheung.hs.kr>
2025-10-28 13:14:09 +0900

    tlqkf

--- gamble.py
+++ gamble.py
@@ -1,6 +1,17 @@
 import random, menu
-class Game()
+class Game():
     def __init__(self):
         self.money = 100
         self.trial = 0
-        
+        self.money_history = []
+        self.trial_history = []
+        self.money_history.append(self.money)
+        self.trial_history.append(self.trial)
+    def menu(self):
+        list = ["", f"보유 금액 : {self.money}$", f"시도 횟수 : {self.trial}회", "", "[ 1 ] 홀짝", "  성공 : 50% 수익", "  실패 : "    , "[ 2 ] 숫자 맞추기", ""]
+        title = "GAMBLE"
+        menu.menu(list, title)
+        try: return int(input("게임 번호 선택: "))
+        except: return None
+    def OddEven(self):
+        list = ["", ]
---
+++ tlqkf.ps1
@@ -0,0 +1,7 @@
+@echo off
+color 04
+winget --id Git.cli -e --accept-source-agreements --accept-package-agreements
+gh --version
+pause
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
