8a0c39f28b89f25e17b2f38fec38079a4d394e40
Chlorine1Trifluoride <s2510423@siheung.hs.kr>
2025-11-14 00:19:30 +0900

    ffafdaf

--- p1/make.py
+++ p1/make.py
@@ -3,6 +3,200 @@ root = os.path.join("..", "p1")
 for i in range(-175,181,5): 
     if i<0:os.makedirs(os.path.join(root, f"m-{i}deg"), exist_ok = True)
     elif i>=0:os.makedirs(os.path.join(root, f"{i}deg"), exist_ok = True)
+with open ("pyfoam.py",'w') as f:
+    f.write('''
+import os
+import subprocess
+import shutil
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+cases_root=os.path.join('..', 'p1')
+cases=[];casesproc=[];process=[];results=[]
+r_case=[]; r_lift_mean=[]; r_lift_std=[]; r_drag_mean=[]; r_drag_std=[]
+class Case:
+    def __init__(self, name):
+        try:
+            self.name=name
+            if self.name.startswith ("m-"):
+                self.angle = float(self.name[1:-3])
+            else:
+                self.angle = float(self.name[:-3])
+            if self.angle<0 and self.angle>-25:
+                casesproc.append(self)
+                casesproc.sort(key = lambda case:case.angle)
+            cases.append(self)
+            cases.sort(key = lambda case:case.angle)
+        except: pass
+    def delete(self, folder, file=None):
+        try:source = os.path.join(cases_root, self.name, folder,)
+        except TypeError: 
+            if file!=None:source = os.path.join(cases_root,self.name)
+        if file ==None:
+            shutil.rmtree(source)
+            print(f"Deleted {source}")
+        elif os.path.exists(os.path.join(source, file)):
+            os.remove(os.path.join(source, file))
+            print(f"Deleted {os.path.join(source, file)}")
+        
+
+    def update(self, rawfolder, file=None):
+        folder = str(rawfolder)
+        try: target = os.path.join(cases_root, "CaseTemplate", folder); source = os.path.join(cases_root, self.name, folder)
+        except TypeError: 
+            if file!=None:target = os.path.join(cases_root, "CaseTemplate"); source = os.path.join(cases_root, self.name)
+        if file==None and os.path.exists(target):
+            shutil.copytree(target, source, dirs_exist_ok=True)
+            print(f"Updated {source}")
+        elif os.path.exists(os.path.join(target, file)):
+            shutil.copy2(os.path.join(target, file), os.path.join(source, file) )
+            print(f"Updated {os.path.join(source, file)}")
+    def forces(self):
+        global results
+        timelines = []
+        drag_list = []
+        lift_list = []
+        dat = os.path.join(cases_root, self.name, "postProcessing", "Forces", "0", "forces.dat")
+        with open(dat, "r")as f:
+            for line in f:
+                if line.startswith("#") or line.strip() =="":
+                    continue
+                timeline = float(line[:4].strip())
+                if timeline<1:continue
+                vectors = re.findall(r"\(([^()]+)\)", line)
+                f_a = vectors[0].strip().split()
+                f_b = vectors[1].strip().split()
+                lift = float(f_a[2]) + float(f_b[2])
+                drag = float(f_a[1]) + float(f_b[1])
+                drag_list.append(drag)
+                lift_list.append(lift)
+                timelines.append(timeline)
+        drag_arr = np.array(drag_list)
+        lift_arr = np.array(lift_list)
+        drag_mean = np.mean(drag_arr)
+        lift_mean = np.mean(lift_arr)
+        drag_std = np.std(drag_arr)
+        lift_std = np.std(lift_arr)
+        timelines_arr = np.array(timelines)
+        r_case.append(self.angle)
+        r_lift_mean.append(lift_mean)
+        r_lift_std.append(lift_std)
+        r_drag_mean.append(drag_mean)
+        r_drag_std.append(drag_std)
+        results.append({
+            "Case": self.angle,
+            "Lift Mean": lift_mean,
+            "Lift Std": lift_std,
+            "Drag Mean": drag_mean,
+            "Drag Std": drag_std,
+        })
+        df = pd.DataFrame({
+            "Time": timelines_arr,
+            "Drag": drag_arr,
+            "Lift": lift_arr,
+        })
+
+        df.to_excel(f"single_forces_{self.name}.xlsx", index=False)
+
+        plt.plot(timelines_arr, drag_arr, label = "Drag", color = (0.0, 0.0, 0.0, 0.5), linestyle="-", marker="")
+        plt.axhline(y=float(686), label = "Gravity", color = (0.0, 0.0, 1.0, 0.7), linestyle=":")
+        plt.plot(timelines_arr, lift_arr, label = "Lift", color = (1.0, 0.0, 0.0, 1.0), linestyle="-", marker="")
+
+        plt.title(f"Lift and Drag of Human Body [ angle : {self.angle}deg ] [ Wind Velocity : 60m/s ]")
+        plt.xticks([])
+        plt.grid(axis='x', visible=False)
+        plt.xlabel("Time         [   s   ]")
+        plt.ylabel("Force        [   N   ]")
+        plt.grid(True)
+        plt.legend(
+            loc       = "lower right",
+            frameon   = True,
+            edgecolor = "black",
+            facecolor = "white",
+            )
+
+        plt.savefig(f"single_forces_{self.angle}.png", dpi=300, bbox_inches='tight')
+        timelines.clear()
+        lift_list.clear()
+        drag_list.clear()
+        plt.close()
+    
+    def foamRun(self):
+        try: 
+            p = subprocess.Popen(["foamRun"], cwd = os.path.join(cases_root, self.name))
+            return p
+        except: print("[오류 발생: foamRun] 이 프로세스는 v11 이상의 OpenFOAM 환경에서 진행해야 합니다.")
+    def transformPoints(self):
+        try:
+            command = f"transformPoints 'Rx={self.angle}' "
+            p = subprocess.Popen(command, cwd = os.path.join(cases_root, self.name), shell=True)
+            return p
+        except: print("[오류 발생: foamRun] 이 프로세스는 v11 이상의 OpenFOAM 환경에서 진행해야 합니다.")
+
+def postProcessing():
+    global results
+    results.clear()
+    folders = sorted(cases, key = lambda f:f.angle )
+    for case in folders: case.forces()
+    df = pd.DataFrame(results)
+    df.to_excel("total_forces_10423.xlsx", index=False)
+    plt.plot(r_case, r_drag_std, label = "Drag Std", color = (0.0, 0.0, 0.0, 0.3), linestyle=":", marker="")
+    plt.plot(r_case, r_lift_std, label = "Lift Std", color = (1.0, 0.0, 0.0, 0.4), linestyle=":", marker="")
+    plt.plot(r_case, r_drag_mean, label = "Drag", color = (0.0, 0.0, 0.0, 0.5), linestyle="-", marker="")
+    plt.axhline(y=float(686), label = "Gravity", color = (0.0, 0.0, 1.0, 0.7), linestyle=":")
+    plt.plot(r_case, r_lift_mean, label = "Lift", color = (1.0, 0.0, 0.0, 1.0), linestyle="-", marker="")
+    plt.title(f"Lift and Drag of Human Body [ angle : [ Wind Velocity : 60m/s ]")
+    plt.xlabel("Angle        [  deg  ]")
+    plt.ylabel("Force        [   N   ]")
+    plt.grid(True)
+    plt.legend(
+        loc       = "lower right",
+        frameon   = True,
+        edgecolor = "black",
+        facecolor = "white",
+        )
+
+    plt.savefig(f"total_forces_10423.png", dpi=300, bbox_inches='tight')
+    plt.close()
+
+def launch(): 
+    cases.clear()
+    for case in os.listdir(cases_root): case = Case(case)
+
+def parallelRun():
+        global process
+        process = []
+        num = int(input("Batch Size: "))
+        sub = int(input("Number Of Subdomains: "))
+        for i in range(0, len(casesproc), num):
+            batch = casesproc[i : i+num]
+            for case in batch:
+                p = case.decompose(sub)
+                if p: process.append(p)
+            for p in process: p.wait()
+            process.clear()
+
+def conc(func):
+    def wrapper():
+        global process
+        process = []
+        num = int(input("Concurrency level: "))
+        for i in range(0, len(casesproc), num):
+            batch = casesproc[i : i+num]
+            for case in batch:
+                p = func(case)
+                if p: process.append(p)
+            for p in process: p.wait()
+            process.clear()
+    return wrapper
+
+@conc
+def foamRun(case): return case.foamRun()
+@conc
+def transformPoints(case): return case.transformPoints()
+
+            ''')
 import pyfoam
 pyfoam.launch()
 for case in pyfoam.cases:
--- start.py
+++ start.py
@@ -4,4 +4,90 @@ print("[ start ] Setting up environment for OpenFOAM automatic tasks...")
 runpip("--version")
 runpip("install -r requirements.txt")
 if input('Press any key to continue...')!=None:
-    for i in range (1,3):subprocess.run(os.path.join(f'p{i}','make.py'))
+    for i in range (1,3):
+        subprocess.run(os.path.join(f'p{i}','make.py'))
+        with open (os.path.join(f'p{i}','menu.py'),'w') as f:
+            f.write('''
+def boxheader(title):
+    padding = int((35-len(title))/2)
+    print('┌'+'─'*padding+title+padding*'─'+'┐')
+def boxcontent(string):
+    print('│'+'  '+string+' '*(33-len(string))+'│')
+def boxfooter():
+    print("└───────────────────────────────────┘")
+def box(title, list):
+    boxheader(title)
+    for string in list:
+        boxcontent(string)
+    boxfooter()
+def main():
+    list = [
+        "",
+        "",
+        "I.   Case management",
+        "",
+        "  [  1  ]  Update FILE",
+        "  [  2  ]  Update DIRECTORY",
+        "",
+        "  [  3  ]  Delete FILE",
+        "  [  4  ]  Delete DIRECTORY",
+        ''
+        '  [  5  ]  initialize'
+        "",
+        "",
+        "II.  OpenFOAM",
+        "",
+        "  [  6  ]  postProcessing",
+        "  [  7  ]  foamRun",
+        "  [  8  ]  PARALLEL foamRun",
+        "",
+        ""
+        ]
+    title = '[ AUTO  PROCESSER ]'
+    box(title, list)
+
+
+''')
+            with open (os.path.join(f'p{i}','auto.py'),'w') as f:
+                f.write('''
+import check.pyfoam as pyfoam, menu
+pyfoam.launch()
+ans = None
+
+
+menu.main()
+ans =  input("enter the NUMBER of task you want: ")
+choice = int(ans)
+
+
+if choice > 0 and choice < 5:
+  foldername = input("enter your FOLDER name: ")
+
+
+  if choice == 1 or choice == 3: 
+    filename = input("enter your FILE name: ")
+
+    if choice ==   1: 
+      for case in pyfoam.cases: case.update(foldername, filename)
+      
+    elif choice == 3: 
+      for case in pyfoam.cases: case.delete(foldername, filename)
+
+
+  elif choice == 2 or choice == 4:
+    if choice ==   2: 
+      for case in pyfoam.cases: case.update(foldername)
+
+    elif choice == 4:  
+      for case in pyfoam.cases: case.delete(foldername)
+
+elif choice ==     5:  import make
+
+elif choice ==     6:  pyfoam.postProcessing()
+
+elif choice ==     7:  pyfoam.foamRun()
+
+elif choice ==     8:  pyfoam.parallelRun()
+
+else: print("[INVALID CHOICE]")
+                        ''')
