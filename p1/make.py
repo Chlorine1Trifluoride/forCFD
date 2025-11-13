9e9f2860c8fb5789772265ae6e4d4e229ffd024f
Chlorine1Trifluoride <s2510423@siheung.hs.kr>
2025-11-14 00:08:00 +0900

    dsafad

--- p1/make.py
+++ p1/make.py
@@ -1,7 +1,8 @@
 import os
 root = os.path.join("..", "p1")
-for i in range(1,61,5): 
-    os.makedirs(os.path.join(root, f"{i}deg"), exist_ok = True)
+for i in range(-175,181,5): 
+    if i<0:os.makedirs(os.path.join(root, f"m-{i}deg"), exist_ok = True)
+    elif i>=0:os.makedirs(os.path.join(root, f"{i}deg"), exist_ok = True)
 import pyfoam
 pyfoam.launch()
 for case in pyfoam.cases:
