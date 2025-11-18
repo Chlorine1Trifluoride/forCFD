cb98442001cfcb6e97f233ea2e7bf28d123dc876
Chlorine Trifluoride <s2510423@siheung.hs.kr>
2025-11-18 06:05:18 +0000

    asdfsf

--- SetUp/Linux/CFD.sh
+++ SetUp/Linux/CFD.sh
@@ -6,6 +6,7 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-sudo apt install -y openfoam code
+sudo apt install -y openfoam
+sudo apt install -y code
 source /opt/openfoam*/etc/bashrc
 echo "source /opt/openfoam*/etc/bashrc" >> ~/.bashrc
